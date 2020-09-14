89cff63d97da26e1a05e62c6aa373e51cdb3e7f8
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-14 17:26:07 +0000

    update Header.js

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return(
+      <div className="header">
+      
+        <img className="header__logo" src="" />
+        
+        <div className="header__nav">
+          <span className="header__navOption">Home</span>
+        <div>
+        
+        <div className="header__nav">
+          <span className="header__navOption">About Me</span>
+        <div>
+        
+        <div className="header__nav">
+          <span className="header__navOption">My Projects</span>
+        <div>
+        
+        <div className="header__nav">
+          <span className="header__navOption">Contact Me</span>
+        <div>
+      </div>
+    );
+}
+
+export default Header;
