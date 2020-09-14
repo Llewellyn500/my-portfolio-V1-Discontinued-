a2ea9502b80e803b8767477208306db784d40512
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-14 17:51:28 +0000

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,36 @@
 import React from "react";
 import "./Header.css";
 import Logo from "./logo.png";
-import { Link } from "react-router-dom";
 
 function Header() {
   return(
       <div className="header">
-      <Link to="/">
+    
         <img className="header__logo" src={Logo} alt="My logo" />
-      </Link>
       
-      <Link to="#">
+      
+      
         <div className="header__nav">
           <span className="header__navOption">Home</span>
         </div>
-      </Link>
+     
       
-      <Link to="#">
+      
         <div className="header__nav">
           <span className="header__navOption">About Me</span>
         </div>
-      </Link>
-      
-      <Link to="#">
+     
+     
         <div className="header__nav">
           <span className="header__navOption">My Projects</span>
         </div>
-      </Link>
+    
       
-      <Link to="#">
+      
         <div className="header__nav">
           <span className="header__navOption">Contact Me</span>
         </div>
-      </Link>
+      
       </div>
     );
 }
