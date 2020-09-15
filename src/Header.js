d29f54ded6061e32469695f6569d5a55a285274c
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-15 17:54:21 +0000

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,19 +3,28 @@ import "./Header.css";
 import Logo from "./logo.png";
 
 function Header() {
+  state = {
+    toggle: false
+  }
+  
+  menuToggle = () =>{
+    this.setState({toggle: !this.state.toggle})
+  }
+  
   return(
+    const {toggle} = this.state;
       <div className="header">
     
         <img className="header__logo" src={Logo} alt="My logo" />
       
-      <ul>
+      <ul className={toggle ? "toggle" : ""}>
         <li><a href="/">Home</a></li>
         <li><a href="/">About Me</a></li>
         <li><a href="/">Projects</a></li>
         <li><a href="/">Contact Me</a></li>
-        <li className="close">X</li>
+        <li className="close" onClick={this.menuToggle}>X</li>
       </ul>
-      <div className="menu">Menu</div>
+      <div className="menu" onClick={this.menuToggle}>Menu</div>
       </div>
     );
 }
