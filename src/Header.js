b11efb038c422f3ea7ee861134ef6d542933c07a
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-15 17:59:17 +0000

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -12,7 +12,7 @@ function Header() {
   }
   
   return(
-    function {toggle} = this.state;
+    function (toggle) = this.state;
       <div className="header">
     
         <img className="header__logo" src={Logo} alt="My logo" />
