90cb50854ad78a777c5e0faddf01871c91c2462d
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-16 19:39:34 +0000

    update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
           <li><a>Projects</a></li>
           <li><a>Contact Me</a></li>
         </ul>
-        <i class="fas fa-bars"></i>
+        <i className="burger" class="fas fa-bars"></i>
       </nav>
     </div>
     );
