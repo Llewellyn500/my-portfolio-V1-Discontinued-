aa9abf977af6bb9872e9f9e184037477d2debe4d
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-16 03:40:24 +0000

    update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 
 function Navbar() {
   return(
-    <div className="navbar">
+    <div className="nav">
       <nav>
         <div>
           <img className="navbar__logo" src={Logo} alt="My logo" />
