aa4ec76d62bca3d390e57f031ad0b5b77b119d83
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-16 03:17:38 +0000

    update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,32 +1,16 @@
 import React from "react";
-import "./Header.css";
-import Logo from "./logo.png";
+import Logo from "./logo.png"
 
-function Header() {
-  state = {
-    toggle: false
-  }
-  
-  menuToggle = () =>{
-    this.setState({toggle: !this.state.toggle})
-  }
-  
+function Navbar() {
   return(
-    const {toggle} = this.state;
-      <div className="header">
-    
-        <img className="header__logo" src={Logo} alt="My logo" />
-      
-      <ul className={toggle ? "toggle" : ""}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/">About Me</a></li>
-        <li><a href="/">Projects</a></li>
-        <li><a href="/">Contact Me</a></li>
-        <li className="close" onClick={this.menuToggle}>X</li>
-      </ul>
-      <div className="menu" onClick={this.menuToggle}>Menu</div>
-      </div>
+    <div className="navbar">
+      <nav>
+        <div className="navbar__logo">
+          <img src={Logo} alt="My logo" />
+        </div>
+      </nav>
+    </div>
     );
 }
 
-export default Header;
+export default Navbar;
