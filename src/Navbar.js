f468f08a2ef33cbdddfde8e995a0c8ab7e058823
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-17 10:34:35 +0000

    update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,23 +9,24 @@ function Navbar() {
             className="logo" 
             src={Logo} 
             alt="My logo" 
-            />
-            
-        <div className="nav__links">
-          <span className="nav__option">Home</span>
-        </div>
-        
-         <div className="nav__links">
-          <span className="nav__option">About Me</span>
-        </div>
-        
-         <div className="nav__links">
-          <span className="nav__option">Projects</span>
-        </div>
+        />
         
          <div className="nav__links">
           <span className="nav__option">Contact Me</span>
         </div>
+        
+        <div className="nav__links">
+          <span className="nav__option">Projects</span>
+        </div>
+        
+          <div className="nav__links">
+          <span className="nav__option">About Me</span>
+        </div>
+        
+        <div className="nav__links">
+          <span className="nav__option">Home</span>
+        </div>
+        
         <i className="fas fa-bars burger"></i>
     </div>
     );
