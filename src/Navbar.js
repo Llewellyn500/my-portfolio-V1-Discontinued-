7d30d67e3e9fa2bab4eb6bb71a74eaec756181ca
llewellyn500 <llewellynpaintsil34@gmail.com>
2020-09-22 10:47:14 +0000

    tested something

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,26 +3,30 @@ import Logo from "./logo.png";
 import "./Navbar.css";
 
 function Navbar() {
-  return(
+  return (
     <div className="nav">
-    <input type="checkbox" id="check"/>
+      <input type="checkbox" id="check" />
       <label for="check" class="btn">
         <i class="fas fa-bars"></i>
       </label>
-    
-          <img
-            className="logo" 
-            src={Logo} 
-            alt="My logo" 
-        />
-        <ul>
-       <li><a href="#">Home</a></li>
-       <li><a href="#">About me</a></li>
-       <li><a href="#">Projects</a></li>
-       <li><a href="#">Contact Me</a></li>
+
+      <img className="logo" src={Logo} alt="My logo" />
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About me</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact Me</a>
+        </li>
       </ul>
     </div>
-    );
+  );
 }
 
 export default Navbar;
